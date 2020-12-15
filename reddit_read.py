03a2845da2b288e799341819d5d1d44325a908aa
spencejax21 <spencejax21@gmail.com>
2020-12-15 11:24:56 -0500

    Add Heroku Postgres support

--- reddit_read.py
+++ reddit_read.py
@@ -6,22 +6,41 @@ from datetime import datetime
 import psycopg2
 import requests
 
-#os.environ['DATABASE_URL'] = "postgres://idhumhpdxrelqx:a759859762e595ce06f2fa14f9a192fc05c6de5f7885060ae0320f2d60ab47ea@ec2-54-161-58-21.compute-1.amazonaws.com:5432/d52rjkrjkl9eau"
-#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
-#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#conn = psycopg2.connect(database = 'postgres', user='postgres', password='Airforce@2020', host='localhost')
+
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit('animemes')
 
 def get_already_posted():
     
+    #already_posted = []
+    #if os.path.isfile("already_posted.txt"):
+        #with open("already_posted.txt","r") as f:
+            #already_posted = (f.read()).split("\n")
+            #already_posted = list(filter(None, already_posted))
+    #return already_posted
     already_posted = []
-    if os.path.isfile("already_posted.txt"):
-        with open("already_posted.txt","r") as f:
-            already_posted = (f.read()).split("\n")
-            already_posted = list(filter(None, already_posted))
-    return already_posted
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute("SELECT post_id FROM posts")
+        row = cur.fetchone()
+
+        while row is not None:
+            already_posted.append(row)
+            print(already_posted)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        return already_posted
 
 def get_hot_posts(subreddit):
 
@@ -52,9 +71,18 @@ def post_filter(hot_posts):
 
 def write_posted(post):
 
-    if os.path.isfile("already_posted.txt"):
-        with open("already_posted.txt","a") as f:
-            f.write("\n" + post)
+    #if os.path.isfile("already_posted.txt"):
+        #with open("already_posted.txt","a") as f:
+            #f.write("\n" + post)
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO posts (post_id) VALUES (%s) RETURNING post_number", (post,) )
+    post_number = cur.fetchone()[0]
+
+    conn.commit()
+    cur.close()
+    conn.close()
  
 def get_post_url(post):
     
@@ -68,13 +96,16 @@ def upload(post_list):
         for x in post_list: 
             if(os.path.isfile(str(x[6]) + ".jpg")):
                 print("Posting " + x[1] + "...")
-                post(str(x[6]) + ".jpg", x[1] + "\nOP: u/" + x[3] + "\n#animemes #anime #memes #funny #funnymemes")
+                try:
+                    post(str(x[6]) + ".jpg", x[1] + "\nOP: u/" + x[3] + "\n#animemes #anime #memes #funny #funnymemes")
+                except RuntimeError:
+                    print("Unsupported file format; could not complete process")
         else:
             print("Process complete.")
     
-    for file in os.listdir('./'): 
-        if (file.endswith('.jpg') or file.endswith('jpg.REMOVE_ME')):
-            os.remove(file)
+    #for file in os.listdir('./'): 
+        #if (file.endswith('.jpg') or file.endswith('jpg.REMOVE_ME')):
+            #os.remove(file)
             
 def download_images(post_list):
 
@@ -103,10 +134,8 @@ def get_feed():
     
     post_list = post_filter(get_hot_posts(subreddit))
     if(post_list):
-        try:
-            download_images(post_list)
-            upload(post_list)
-        except RuntimeError:
-            print("Unsupported file format; could not complete process")
 
+        download_images(post_list)
+        upload(post_list)
+        
 get_feed()
